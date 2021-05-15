2e34bc8ef476cc6fde13c0b0d8195cd654fa5480
Pradhum Bansal <bansal.pradhumfeb@gmail.com>
2021-05-15 20:37:43 +0530

    1.10

--- src/App.js
+++ src/App.js
@@ -6,34 +6,29 @@ const App = ()=> {
   const [bad, setBad] = useState(0)
 
 
-  const goodClick = () =>
-  {
-    setGood(good+1)
-  }
-  const neutralClick = () =>
-  {
-    setNeutral(neutral+1)
-  }
-  
-  const badClick = () =>
-  {
-    setBad(bad+1)
-  }
-  const course =
-  {
-    name : 'Give Feedbaack'
-  }
+  const goodClick = () =>setGood(good+1)
+  const neutralClick = () =>setNeutral(neutral+1)
+  const badClick = () =>setBad(bad+1)
+  const course ={name : 'Give Feedbaack'}
   return(
     <div>
       <h1> {course.name}</h1>
-      <button onClick = {goodClick}> Good</button>
-      <button onClick = {neutralClick}>Neutral</button>
-      <button onClick = {badClick}>Bad</button>
+      <Button click = {goodClick}text = "Good"/>
+      <Button click = {neutralClick}text = "Neutral"/>
+      <Button click = {badClick}text = "Bad"/>
       <h1> Statistics </h1>
       <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     </div>
   );
   }
+  const Button =(props) =>
+  {
+    return( <button onClick = {props.click} type = "button">{props.text}</button>
+
+    )
+  } 
+
+
   const Statistics = (props) =>{
       let{good,neutral,bad} = props
       if(good+bad+neutral === 0)
@@ -46,13 +41,23 @@ const App = ()=> {
     
     return(
     <div>
-      good: {good}<br/>
-      neutral: {neutral}<br/>
-      bad: {bad}
+      <Statistic name ="good" value= {good}/><br/>
+      <Statistic name = "neutral" value= {neutral}/><br/>
+      <Statistic name = "bad" value = {bad}/>
       <p>all : {good+bad+neutral} </p>
       <p>average :{(good-bad)/(good+bad+neutral)} </p>
       <p>percentage :{(good*100)/ (good+bad+neutral)}% </p>
     </div>
     )   
+    }
+
+    const Statistic = (props) =>
+    {
+      return(
+        <div>
+          {props.name} : {props.value}
+        </div>
+        
+      )
     }
 export default App;
