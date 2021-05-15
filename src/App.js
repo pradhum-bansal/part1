9bea98ea1a671f941028e5496f819a199b6c2a64
Pradhum Bansal <bansal.pradhumfeb@gmail.com>
2021-05-15 23:01:32 +0530

    anectodes1

--- src/App.js
+++ src/App.js
@@ -1,65 +1,23 @@
 import React, { useState } from 'react'
-const App = ()=> {
 
-  const [good, setGood] = useState(0)
-  const [neutral,setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
+  
 
-  const goodClick = () =>setGood(good+1)
-  const neutralClick = () =>setNeutral(neutral+1)
-  const badClick = () =>setBad(bad+1)
-  const course ={name : 'Give Feedbaack'}
-  return(
+  return (
     <div>
-      <h1> {course.name}</h1>
-      <Button click = {goodClick}text = "Good"/>
-      <Button click = {neutralClick}text = "Neutral"/>
-      <Button click = {badClick}text = "Bad"/>
-      <h1> Statistics </h1>
-      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
+      {anecdotes[selected]}
+      <button onClick = {()=>{setSelected( Math.floor(Math.random() *6))}}>click</button>
     </div>
-  );
-  }
-  const Button =(props) =>
-  {
-    return( <button onClick = {props.click} type = "button">{props.text}</button>
-
-    )
-  } 
-
-
-  const Statistics = (props) =>{
-      let{good,neutral,bad} = props
-      if(good+bad+neutral === 0)
-      return(
-        <div>
-          No feedback given.
-        </div>
-      )
-    else
-    
-    return(
-    <div>
-      
-      <Statistic name ="good" value= {good}/> 
-      <Statistic name = "neutral" value= {neutral}/>
-      <Statistic name = "bad" value = {bad}/>
-     <tr> <td>all</td> <td>{good+bad+neutral} </td></tr>
-    <tr>  <td>average</td><td>{(good-bad)/(good+bad+neutral)} </td></tr>
-    <tr>  <td>percentage </td><td>{(good*100)/ (good+bad+neutral)}% </td></tr>
-      
-    </div>
-    )   
-    }
-
-    const Statistic = (props) =>
-    {
-      return(
-        <tr>
-         <td> {props.name}</td> <td> {props.value}</td>
-        </tr>
-        
-      )
-    }
-export default App;
+  )
+}
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+export default App
